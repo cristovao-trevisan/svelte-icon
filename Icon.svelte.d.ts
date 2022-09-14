5dcb7091699d473ab98cf429b2dc0e35af4d18ba
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2022-09-14 17:13:23 -0300

    Update types

--- Icon.svelte.d.ts
+++ Icon.svelte.d.ts
@@ -1,6 +1,19 @@
-import { SvelteIconProps } from 'svelte-icon';
+import type { SvelteComponentTyped } from 'svelte';
 
-export default class Icon {
-	$$prop_def: SvelteIconProps;
+export interface SvelteIconProps {
+	data: string
+	viewBox?: string
+	
+	size?: string
+	width?: string
+	height?: string
+
+	color?: string
+	stroke?: string
+	fill?: string
+
+	[key: string]: unknown
 }
+
+export default class Icon extends SvelteComponentTyped<SvelteIconProps> {}
 
--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,7 @@
+import type { SvelteComponentTyped } from 'svelte';
+import type { SvelteIconProps } from './Icon.svelte';
+
 declare module "svelte-icon" {
-  export interface SvelteIconProps {
-    data: string
-    viewBox?: string
-    
-    size?: string
-    width?: string
-    height?: string
 
-    color?: string
-    stroke?: string
-    fill?: string
-
-    [key: string]: unknown
-  }
-
-  export default class Icon {
-    $$prop_def: SvelteIconProps
-  }
+  export default class Icon extends SvelteComponentTyped<SvelteIconProps> {}
 }
